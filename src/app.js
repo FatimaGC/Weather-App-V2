e99e293181d0ba13426b952830c1aa6e536fecb9
Fatima Guzman <fguzman1230@gmail.com>
2022-04-17 17:55:51 -0600

    Added JS to update the alt info according to location

--- src/app.js
+++ src/app.js
@@ -70,6 +70,8 @@ function showTemperature(response) {
     "src",
     `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
   );
+
+  icon.setAttribute("alt", response.data.weather[0].description);
 }
 
 function fetchTemperature(queryParameters) {
